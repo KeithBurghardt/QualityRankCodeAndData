477bd3d71f65d9b239d620ccb3966c9591739927
Keith Burghardt <keithab@isi.edu>
2019-11-22 18:15:53 -0800

    Delete amt_keys.php

--- Experiment Code/task_with_guess/amt_keys.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Initialize amt\request class
- * @author cpks
- * @license Public Domain
- */
-amt\request::init_class('AKIAJIAXJXVGCDXALMXQ', 'xc6F7h+lfbhgxHhnX9gqnB8+jhRIcAP/Gq7042ME');
